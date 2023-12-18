99a53bbfff3c2df9d2e544692d9b34ecbb462a08
yanasaa <yanasaa@gmail.com>
2023-12-18 23:27:18 +0300

    fix: getDistanceBetweenPoints func

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -74,8 +74,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt(Math.abs(x2 - x1) ** 2 + Math.abs(y2 - y1) ** 2);
 }
 
 /**
